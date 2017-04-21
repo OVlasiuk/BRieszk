dc4d65931d4b96368742912433142ef0ca294e3d
OVlasiuk <oleksandr.vlasiuk@vanderbilt.edu>
2017-04-21 16:18:39 -0500

    Precode the power function for s=0.5 and s=5.0

--- riesz_sphere.m
+++ riesz_sphere.m
@@ -1,18 +1,22 @@
-function cnf = riesz_sphere(cnf,N,dim,s)
+function cnf = riesz_sphere(cnf,N,dim,s,plotit)
 %RIESZ_SPHERE
 % cnf = riesz_sphere(cnf,N,dim,s)
 % Returns a configuration obtained from applying the gradient descent to
 % the given (or random) N-point collection on the unit sphere.
 % 
-% cnf -- pass your initial configuration as a matrix (dim)x(#of points)
-%   here; 
+% cnf -- pass your initial configuration as a matrix (dim)x(#of points); 
 %   pass ZERO to use all default settings;
 %   pass ONE to draw from the Gaussian random distribution using your N,dim,s;
 % N -- number of points in the random configuration to be generated
 %   (ignored if an initial cnf is being passed);
 % dim -- dimension of the ambient space; deduced from the first dimension
 %   of the cnf matrix, if any;
-% s -- exponent in the Riesz energy to be minimized; default value is 6.0.
+% s -- exponent in the Riesz energy to be minimized; the default value is
+%   5.0.
+%   It is HIGHLY recommended to use either s=5.0 or s=0.5, as these are 
+%   pre-coded, or to modify the source code. Otherwise you'll be using the 
+%   Matlab's power function, which turns out to be not that great.
+% plotit -- pass 'y' or 1, etc., to plot the produced configuration.
 if cnf==0 || cnf==1
     dim = cnf*dim + (1-cnf)*3;
     s = cnf*s + (1-cnf)*6.0;
@@ -22,8 +26,20 @@ else
     dim = size(cnf,1);
     N = size(cnf,2);
 end
+if s==5.0
+    compute_weights = @(x) 1./x./x./x;
+else
+    if s==0.5
+        compute_weights = @(x) 1./x./sqrt(x);
+    else
+        compute_weights = @(x) sqrt(x).^(-s-1);
+    end
+end
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
 k_value = 20;  
 repel_steps = 200;
+repel_cycles = 10;
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
 fprintf( '\nWe will be minimizing the %3.2f-Riesz energy of %d points on the',s,N)
 fprintf( '\n%d-dimensional unit sphere.\n\n', dim-1)
 % double-check we're on the sphere:
@@ -36,8 +52,7 @@ pbaspect([1 1 1])
 % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
 % plot3(cnf(1,:),cnf(2,:),cnf(3,:),'.k','MarkerSize',8)
 
-
-for cycle=1:10
+for cycle=1:repel_cycles
     [IDX, D] = knnsearch(cnf', cnf', 'k', k_value+1);
     IDX = IDX(:,2:end)';             % drop the trivial first column in IDX
     step = min(D(:,2));
@@ -45,8 +60,8 @@ for cycle=1:10
     for iter=1:repel_steps
         cnf_repeated = reshape(repmat(cnf,k_value,1),dim,[]);
         knn_differences = cnf_repeated - cnf(:,IDX);
-        knn_norms = sqrt(sum(knn_differences.^2,1));
-        riesz_weights = knn_norms.^(-s-1);
+        knn_norms = sum(knn_differences.^2,1);
+        riesz_weights = compute_weights(knn_norms);
         directions = bsxfun(@times,riesz_weights,knn_differences);
         directions = reshape(directions, dim, k_value, []);
         normals = reshape(sum(directions,2), dim, []);
@@ -60,16 +75,17 @@ for cycle=1:10
     end
     toc
 end
+    
 % [IDX, D] = knnsearch(cnf', cnf', 'k', k_value+1);
 % step = min(D(:,2));
-if dim==3
+if dim==3 && (plotit=='y' || plotit=='Y' || plotit==1)
     colormap(winter)
     [x,y,z] = sphere(30);
     mesh(x,y,z)
     hold on
     plot3(cnf(1,:),cnf(2,:),cnf(3,:),'.k','MarkerSize',7)
 else
-    if dim==2
+    if dim==2 && (plotit=='y' || plotit=='Y' || plotit==1)
         plot(cnf(1,:),cnf(2,:),'.k','MarkerSize',4)
     end
 end
