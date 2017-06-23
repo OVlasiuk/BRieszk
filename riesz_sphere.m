a3cee92abe2998f82daf9079e655b954fa7177f1
OVlasiuk <oleksandr.vlasiuk@vanderbilt.edu>
2017-06-23 11:40:13 -0500

    Correct Riesz power and add an option to output it

--- riesz_sphere.m
+++ riesz_sphere.m
@@ -14,7 +14,8 @@ function cnf = riesz_sphere(cnf,N,dim,s,plotit,silent)
 %   of the cnf matrix, if any;
 % s -- exponent in the Riesz energy to be minimized; the default value is
 %   5.0.
-%   It is HIGHLY recommended to use either s=5.0 or s=0.5, as these are 
+% s -- the exponent used in the Riesz kernel;
+%   It is HIGHLY recommended to use either s=4.0 or s=0.5, as these are 
 %   pre-coded, or to modify the source code. Otherwise you'll be using the 
 %   Matlab's power function, which turns out to be not that great.
 % plotit -- pass 'y' or 1, etc., to plot the produced configuration.
@@ -31,22 +32,28 @@ if isscalar(cnf)
     if ~exist('silent','var') || ~silent
         fprintf( '\nStarting with a random point set.')
     end
+    if ~exist('N','var')
+        N = 1000;
+    end
+    if ~exist('dim','var')
+        dim = 3;
+    end
     cnf = randn(dim,N); 
 else
     [dim, N] = size(cnf);
 end
-if ~exist('s','var')
-    s = 5.0;
-end 
+if ~exist('s', 'var')
+    s = 4.0;
+end
 switch s
-    case 5.0
-        compute_weights = @(x) 1./x./x./x;
-    case 2.5
-        compute_weights = @(x) 1./x./sqrt(x)./sqrt(sqrt(x));
+    case 4.0
+        compute_riesz = @(x) 1./x./x;
+    case 2.0
+        compute_riesz = @(x) 1./x;
     case 0.5
-        compute_weights = @(x) 1./sqrt(x)./sqrt(sqrt(x));
+        compute_riesz = @(x) 1./sqrt(sqrt(x));
     otherwise
-        compute_weights = @(x) sqrt(x).^(-s-1);     
+        compute_riesz = @(x) sqrt(x).^(-s);
 end
 % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
 if s < dim
@@ -74,37 +81,31 @@ close all;
 
 for cycle=1:repel_cycles
     if s>=dim || cycle==1
-        [IDX, D] = knnsearch(cnf', cnf', 'k', k_value+1);
+        [IDX, ~] = knnsearch(cnf', cnf', 'k', k_value+1);
         IDX = IDX(:,2:end)';             % drop the trivial first column in IDX
-        step = min(D(:,2));
-    else
-        if cycle<3
-            [~, D] = knnsearch(cnf', cnf', 'k', 2);
-            step = min(D(:,2));
-        end
     end
     tic
     for iter=1:repel_steps*cycle
         cnf_repeated = reshape(repmat(cnf,k_value,1),dim,[]);
         knn_differences = cnf_repeated - cnf(:,IDX);
-        knn_norms = sum(knn_differences.*knn_differences,1);
-        riesz_weights = compute_weights(knn_norms);
-        directions = bsxfun(@times,riesz_weights,knn_differences);
-        directions = reshape(directions, dim, k_value, []);
-        normals = reshape(sum(directions,2), dim, []);
-%         normals = directions./sqrt(sum(directions.^2,1));
-        tangents = normals-bsxfun(@times,sum(cnf.*normals,1),cnf); 
-        % computing scalar products only works because we are on the unit
-        % sphere
+        knn_norms_squared = sum(knn_differences.*knn_differences,1);
+        riesz_weights = compute_riesz(knn_norms_squared)./knn_norms_squared;
+        gradient = bsxfun(@times,riesz_weights,knn_differences);
+        gradient = reshape(gradient, dim, k_value, []);
+        gradient = reshape(sum(gradient,2), dim, []);
+        tangents = gradient-bsxfun(@times,sum(cnf.*gradient,1),cnf); 
+        % computing scalar products like so only works because we are on 
+        % the unit sphere
         tangents = tangents/max(sqrt(sum(tangents.^2,1)));
-        cnf = cnf + tangents * step/(3+cycle);%/iter
+        step = sqrt(min(reshape(knn_norms_squared,k_value,[]),[],1));
+        cnf = cnf + tangents .* step/(3+iter);
         cnf = cnf./sqrt(sum(cnf.*cnf,1));
     end
     if ~exist('silent','var') || ~silent
         toc
     end
 end
-    
+
 % [IDX, D] = knnsearch(cnf', cnf', 'k', k_value+1);
 % step = min(D(:,2));
 msize = ceil(max(1, 22-3.5*log10(size(cnf,2)) ));
@@ -122,5 +123,17 @@ else
         plot(cnf(1,:),cnf(2,:),'.k','MarkerSize',ceil(msize/2))
     end
 end
+
+fprintf('Compute the full Riesz energy of this pointset? [y/N]\n')
+inp = input('','s');
+if ~isempty(inp) && ((inp=='y') || (inp=='Y') || (inp=='1'))
+    en =    (cnf(1,:)-cnf(1,:)').*(cnf(1,:)-cnf(1,:)') +...
+                (cnf(2,:)-cnf(2,:)').*(cnf(2,:)-cnf(2,:)') +...
+                (cnf(3,:)-cnf(3,:)').*(cnf(3,:)-cnf(3,:)');
+    en = compute_riesz(en);
+    en = sum(sum(en(isfinite(en))));
+    fprintf('The %3.2f-Riesz energy is \t %10.6f\n', s,en)
+end
+
 
 % dlmwrite('cnf.out',cnf','delimiter','\t'); % ,'precision',3)
